5e993813120f466f7bd65417494518da8e5a4ee6
finetuning-xh <hatem.xhezairi@finetuning.it>
2020-01-15 15:37:48 +0100

    First Step

---
+++ db/schema.rb
@@ -0,0 +1,22 @@
+
+ActiveRecord::Schema.define(version: 2020_01_15_143312) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "posts", "users"
+
+end
