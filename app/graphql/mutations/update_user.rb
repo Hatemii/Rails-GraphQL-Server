97c5bec4860542f6297ae2e8502feba0bcacd915
finetuning-xh <hatem.xhezairi@finetuning.it>
2020-01-16 16:46:12 +0100

    name:,email: For User

--- app/graphql/mutations/update_user.rb
+++ app/graphql/mutations/update_user.rb
@@ -1,15 +1,16 @@
 class Mutations::UpdateUser < Mutations::BaseMutation
 
-      # argument :id, Integer, required: false
+      argument :id, Integer, required: false
       argument :name, String, required: false
       argument :email, String, required: false
 
       field :user, Types::UserType, null: false
       field :errors, [String], null: false
 
-        def resolve(name:)
+        def resolve(name:,email:)
           user = User.first
           user.name=name
+          user.email=email
           user.save
           {user: user}
         end
