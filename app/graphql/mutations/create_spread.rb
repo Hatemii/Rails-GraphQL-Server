be106770d40527c6b7fc0e80eb6369b2776b98b5
Hatemii <hatem.xhezairi96@gmail.com>
2020-05-06 11:53:00 +0200

    Create spread with User or Group

--- app/graphql/mutations/create_spread.rb
+++ app/graphql/mutations/create_spread.rb
@@ -3,15 +3,22 @@
 class Mutations::CreateSpread < Mutations::BaseMutation
 
   argument :attributes, InputTypes::SpreadInputType, required: true
-  argument :id, ID, required: false
+  argument :spread_id, ID, required: false
+  argument :user_id, ID, required: false
+  argument :group_id, ID, required: false
 
-
-  def resolve(id:nil,attributes:)
-    if id
-      Spread.find(id).update!(attributes.to_h)
-      Spread.find(id)
+  def resolve(spread_id:nil,attributes:,user_id:nil,group_id:nil)
+    if spread_id
+      Spread.find(spread_id).update!(attributes.to_h)
+      Spread.find(spread_id)
     else
-      Spread.create!(attributes.to_h)
+      if user_id
+        User.find(user_id).spreads.create!(attributes.to_h)
+      elsif group_id
+        Group.find(group_id).spreads.create!(attributes.to_h)
+      else
+        Spread.create!(attributes.to_h)
+      end
     end
   end
 end
