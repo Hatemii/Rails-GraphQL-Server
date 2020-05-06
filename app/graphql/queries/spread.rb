0214111ded07dc59fce18e6f95753f74f8d444fb
Hatemii <hatem.xhezairi96@gmail.com>
2020-05-06 11:59:37 +0200

    queries by User or Group

--- app/graphql/queries/spread.rb
+++ app/graphql/queries/spread.rb
@@ -3,20 +3,23 @@ module Queries
 
      type [OutputTypes::SpreadType], null: false
 
-     argument :id, ID, required: false
+     argument :spread_id, ID, required: false
      argument :user_id, ID, required: false
      argument :group_id, ID, required: false
 
 
-    def resolve(id: nil,user_id:nil,group_id:nil)
-      if id
-        ::Spread.where(id: id)
+    def resolve(spread_id: nil,user_id:nil,group_id:nil)
+      if spread_id
+        ::Spread.where(spread_id: spread_id)
 
+      elsif user_id
+        ::User.find(user_id).spreads.all.order("id ASC")
+
+      elsif group_id
+        ::Group.find(group_id).spreads.all.order("id ASC")
       else
-        ::Spread.all
-
+        ::Spread.all.order("id ASC")
       end
     end
-
   end
 end
