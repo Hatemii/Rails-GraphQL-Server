dc7d04d986e4f749712eb1c6b93d9703e0e95795
Hatemii <hatem.xhezairi96@gmail.com>
2020-05-13 14:50:23 +0200

    iban already exist - dont create new

--- app/graphql/mutations/create_bank_account.rb
+++ app/graphql/mutations/create_bank_account.rb
@@ -3,18 +3,19 @@ class Mutations::CreateBankAccount < Mutations::BaseMutation
   argument :attributes, InputTypes::BankAccountInputType, required: true
   argument :id,ID,required: false
 
-  def resolve(id:nil,attributes:)
 
-    if BankAccount.where(currency:attributes[:currency]).where('DATE(created_at) = ?', Date.today).any?
-      BankAccount.where(currency:attributes[:currency]).last.update!(attributes.to_h)
-    else
-      if attributes.iban.to_i < 100
-        raise GraphQL::ExecutionError, "You dont have enough balance to complete this"
+  def resolve(attributes:)
+
+    iban_values = BankAccount.where(currency:attributes[:currency]).pluck(:iban)
+
+    iban_values.each do |x|
+      if x == attributes[:iban]
+        raise GraphQL::ExecutionError, "iban already exist in array"
       else
         BankAccount.create!(attributes.to_h)
       end
     end
-    BankAccount.where(currency:attributes[:currency]).last
+      BankAccount.where(currency:attributes[:currency]).last
 
   end
 end
