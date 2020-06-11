882fd4a0c9b48aea2fa99efd692df90b78871259
Hatemii <hatem.xhezairi96@gmail.com>
2020-06-11 18:43:39 +0200

    Limit update_column - success:true

--- app/graphql/mutations/create_limit.rb
+++ app/graphql/mutations/create_limit.rb
@@ -1,13 +1,20 @@
 class Mutations::CreateLimit < Mutations::BaseMutation
 
-  argument :attributes, InputTypes::LimitInputType, required: true
   argument :limit_id, ID, required: false
+  argument :user_id, ID, required: false
+  argument :limit, String, required: true
 
-  def resolve(limit_id:nil,attributes:)
+
+  def resolve(limit_id:nil, user_id:nil, limit:)
 
     if limit_id
-      Limit.find(limit_id).update!(attributes.to_h)
-      Limit.find(limit_id)
+      a_limit = Limit.find(limit_id)
+
+      if a_limit.update_column(:limit, limit)
+        {success:true}
+      else
+        {success:false}
+      end
 
     else
       Limit.create!(attributes.to_h)
