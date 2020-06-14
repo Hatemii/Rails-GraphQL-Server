1b1e1d87498370bff1623a7fbc1873d4a6b548ec
Hatemii <hatem.xhezairi96@gmail.com>
2020-06-14 23:30:03 +0200

    BankAccount primary - already true!

--- app/graphql/mutations/create_bank_account.rb
+++ app/graphql/mutations/create_bank_account.rb
@@ -1,19 +1,34 @@
 class Mutations::CreateBankAccount < Mutations::BaseMutation
 
-  argument :attributes, InputTypes::BankAccountInputType, required: false
+  argument :attributes, InputTypes::BankAccountInputType, required: true
   argument :bank_id,ID,required:false
 
   def resolve(attributes:attributes,bank_id:nil)
 
     if bank_id
-      BankAccount.find(bank_id).update!(attributes.to_h)
-      BankAccount.find(bank_id)
+      bank = BankAccount.find(bank_id)
+      bank_currency = bank.currency
+
+      find_true = BankAccount.where(currency:attributes.currency).where(primary:true).any?
+
+      if find_true == true && attributes.primary == true
+        if bank.primary == true
+          raise GraphQL::ExecutionError, "primary is already true"
+        else
+
+          current_primary = BankAccount.where(currency:bank_currency).where(primary:true)
+          current_primary.update(primary:false)
+        end
+      end
+
+      bank.update!(attributes.to_h)
+      bank.save!
+      bank
 
     else
+      find_true = BankAccount.where(currency:attributes.currency).where(primary:true).any?
 
-      if_primary = BankAccount.where(currency:attributes.currency).where(primary:true).any?
-
-      if if_primary == true && attributes.primary == true
+      if find_true == true && attributes.primary == true
         current_primary = BankAccount.where(currency:attributes.currency).where(primary:true)
         current_primary.update(primary:false)
 
