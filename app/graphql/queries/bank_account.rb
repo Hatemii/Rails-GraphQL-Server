251c90d7747a439325eb9894ebae00a314a08718
Hatemii <hatem.xhezairi96@gmail.com>
2020-06-17 10:29:42 +0200

    query with JSON

--- app/graphql/queries/bank_account.rb
+++ app/graphql/queries/bank_account.rb
@@ -1,17 +1,54 @@
-module Queries
-  class BankAccount < Queries::BaseQuery
+module Queries::MyProfile
+  class Payments < Queries::BaseQuery
 
-     type [OutputTypes::BankAccountType], null: true
-     argument :user_id, ID, required: false
+    type OutputTypes::BankAccountDetailType, null: true
+
+    def resolve
+      user = context[:current_user]
+      user_bank_accounts = Pundit::policy_scope(user, ::BankAccount.all)
 
 
-    def resolve(user_id: nil)
-      if user_id
-        ::BankAccount.where(user_id: user_id).order("id DESC")
-      else
-        ::BankAccount.where(user_id: nil).order("id DESC")
+      payments = []
+      ENABLED_CURRENCY_TRADER_CURRENCIES.each do |currency|
+        bank_accounts = user_bank_accounts.where(currency:currency).select("id", "iban", "primary")
+        payments.push({currency: currency, bank_accounts: bank_accounts})
       end
-    end
 
+      {
+          bank_accounts: payments
+      }
+
+    end
   end
 end
+
+# this is query from caesar-portal
+# 1. query/my_profile/payments.rb
+#
+# 2. policies/bank_account_policy.rb
+# class BankAccountPolicy < ApplicationPolicy
+#   class Scope < Scope
+#     def resolve
+#       if Rails.env == "internal_staging"
+#         if user.id == 17 || user.id == 11
+#           scope.all
+#         else
+#           scope.where(user_id: user.id)
+#         end
+#
+#       elsif Rails.env == "staging"
+#         if user.id == 13 || user.id == 15
+#           scope.all
+#         else
+#           scope.where(user_id: user.id)
+#         end
+#       else
+#         if user.id == 61
+#           scope.all
+#         else
+#           scope.where(user_id: user.id)
+#         end
+#       end
+#     end
+#   end
+# end
